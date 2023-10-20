a753d86e5cbdaac9c714ae9a869bdc3d4f9047e5
LOZONSCHI CONSTANTIN-RAZVAN <310440501RSL211122@feaa.uaic.ro>
2023-10-20 18:14:39 +0300

    frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SignalRClient';
+  private hubConnectionBuilder!: HubConnection;
+  offers: any[] = [];
+  constructor() {
+  }
+  ngOnInit(): void {
+    this.hubConnectionBuilder = new HubConnectionBuilder()
+      .withUrl('https://localhost:7016/offers')
+      .configureLogging(LogLevel.Information)
+      .build();
+    this.hubConnectionBuilder
+      .start()
+      .then(() => console.log('Connection started.......!'))
+      .catch(err => console.log('Error while connect with server'));
+    this.hubConnectionBuilder.on('SendOffersToUser', (result: any) => {
+      this.offers.push(result);
+    });
+}
 }
